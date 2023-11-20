20afa083d5f911b3c3f44127792caa30b126875e
Greg Wilson <gvwilson@third-bit.com>
2023-11-20 13:09:20 -0500

    refactor: moving file collector to utilities

--- lib/mccole/bin/util.py
+++ lib/mccole/bin/util.py
@@ -1,10 +1,40 @@
 """Utilities."""
 
 import importlib.util
+from pathlib import Path
 import sys
+
+from bs4 import BeautifulSoup
 
 
 INDEX_FILE = "index.md"
+
+
+def collect_files(config, which):
+    """Read text of source and output files."""
+
+    def _same(x):
+        return x
+
+    def _parse(x):
+        return BeautifulSoup(x, "html.parser")
+
+    if which == "markdown":
+        root_dir = config.src_dir
+        filename = "index.md"
+        transform = _same
+    elif which == "html":
+        root_dir = config.out_dir
+        filename = "index.html"
+        transform = _parse
+    else:
+        util.fail(f"unknown file type in collector {which}")
+
+    paths = [
+        Path(root_dir, filename),
+        *[Path(root_dir, slug, filename) for slug in config.chapters],
+    ]
+    return {p: transform(p.read_text()) for p in paths}
 
 
 def fail(msg):
